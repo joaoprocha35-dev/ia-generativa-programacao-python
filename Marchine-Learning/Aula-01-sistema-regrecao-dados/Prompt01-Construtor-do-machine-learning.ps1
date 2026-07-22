4f0c0c83c977f446e238fa11031a9c920fbba7ae
gabrielatorres-max <gabriel.a.torres@edu.senai.br>
2026-07-21 21:54:53 -0300

    Adiciona atividade de regressão linear

---
+++ Marchine-Learning/Aula-01-sistema-regrecao-dados/Prompt01-Construtor-do-machine-learning.ps1
@@ -0,0 +1,16 @@
+prompt -1:
+
+Usando a base de dados rotulada disponibilizada crie um Jupyter Notebook em Python que permita treinar um algoritmo de Regressão Linear para prever o salário de um funcionário a partir de 5 características (Nível Profissional, Experiência em Anos, Escolaridade, Avaliação de Desempenho e Certificações Profissionais).
+
+Siga os 4 passos tradicionais do Machine Learning:
+
+1. Ingestão dos dados.
+2. Separação dos dados para treino e teste.
+3. Treinamento do algoritmo de Regressão Linear.
+4. Validação do resultado (R2 e RMSE)
+
+Faça a importaçcão das bibliotecas internas toda na primeira célula do código,depois coloque cada um dos 4 passos acima em uma celula separada faça o codigo mais simples possivei. coloque uma comentario em cima de cada linha de código explicando o que ela faz. e , no final,exporte o modelo de machine learning treinando para que possa ser utilizado posteriormente junto a uma interface gráfica.
+
+Não Prsuma informações,caso tenha duvidas antes de criar o conteúdo faça perguntas para que eu responda, lembre-se que sou eu que mando
+
+
